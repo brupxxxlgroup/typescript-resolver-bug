0ef9041e8a0ff0c5c43cb63c7b508c612bc82dde
Patrick Brunmayr <BRUP@xxxlgroup.com>
2025-08-21 17:53:48 +0200

    Initial commit

---
+++ eslint.config.bug.js
@@ -0,0 +1,21 @@
+const js = require("@eslint/js");
+const { importX } = require("eslint-plugin-import-x");
+const tseslint = require("typescript-eslint");
+
+module.exports = [
+  js.configs.recommended,
+  importX.flatConfigs.recommended,
+  importX.flatConfigs.typescript,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
+    languageOptions: {
+      parser: tseslint.parser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      "import-x/no-dynamic-require": "warn",
+      "import-x/no-nodejs-modules": "warn",
+    },
+  },
+];

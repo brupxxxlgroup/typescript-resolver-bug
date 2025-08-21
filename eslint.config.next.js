ba79fa3627e74d7a193ee481809be25ba1054be5
Patrick Brunmayr <BRUP@xxxlgroup.com>
2025-08-21 18:06:21 +0200

    Update sample

---
+++ eslint.config.next.js
@@ -0,0 +1,28 @@
+const js = require("@eslint/js");
+const { importX } = require("eslint-plugin-import-x");
+const tseslint = require("typescript-eslint");
+const {
+  createTypeScriptImportResolver,
+} = require("eslint-import-resolver-typescript");
+
+module.exports = [
+  js.configs.recommended,
+  importX.flatConfigs.recommended,
+  importX.flatConfigs.typescript,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
+    languageOptions: {
+      parser: tseslint.parser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    settings: {
+      "import-x/resolver": null,
+      "import-x/resolver-next": [createTypeScriptImportResolver()],
+    },
+    rules: {
+      "import-x/no-dynamic-require": "warn",
+      "import-x/no-nodejs-modules": "warn",
+    },
+  },
+];
